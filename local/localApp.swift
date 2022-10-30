df6fc00fc5adcfcd7d2f0f579aafc3c595ef633f
Ziloliddin <Zilolidddinkamolov@gmail.com>
2022-10-30 18:03:47 +0500

    Initial Commit

---
+++ local/localApp.swift
@@ -0,0 +1,17 @@
+//
+//  localApp.swift
+//  local
+//
+//  Created by Macbook Pro 14 Inch on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct localApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
